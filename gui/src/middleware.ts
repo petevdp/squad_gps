a1f91d47f5929167556848fba516e2ed9bc2d4b8
petevdp <pjvanderpol@gmail.com>
2023-08-02 00:30:22 -0700

    add redirect middleware

--- gui/src/middleware.ts
+++ gui/src/middleware.ts
@@ -1,8 +1,10 @@
-import { rewrite } from '@vercel/edge'
+export const config = {
+	matcher: '/:path',
+}
 
-export default function middleware(request: any) {
+export default function middleware(request: Request) {
 	const url = new URL(request.url)
 	if (url.pathname !== '/') {
-		return rewrite('/', request.url)
+		return Response.redirect(new URL('/', request.url))
 	}
 }
