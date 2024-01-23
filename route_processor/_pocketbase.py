09b4231e2d364ba1bd0e43a649c8221b09fa6dda
petevdp <pjvanderpol@gmail.com>
2024-01-23 15:51:57 -0800

    setup caddy, fix login state, wait on exit thread in server

--- route_processor/_pocketbase.py
+++ route_processor/_pocketbase.py
@@ -1,6 +1,6 @@
 from pocketbase import PocketBase
 
-from server import config
+from route_processor import config
 
 pb = PocketBase(config.POCKETBASE_URL)
 pb.admins.auth_with_password(config.POCKETBASE_SERVICE_EMAIL, config.POCKETBASE_SERVICE_PASSWORD)
--- route_processor/_supabase.py
+++ route_processor/_supabase.py
@@ -1,5 +1,5 @@
 from supabase import Client, create_client
 
-from server import config
+from route_processor import config
 
 supabase: Client = create_client(config.SUPABASE_URL, config.SUPABASE_SERVICE_KEY)
